c7ca26760a77492936818a9144e90b87795c314c
Sanchez70 <124312881+Sanchez70@users.noreply.github.com>
2023-07-11 07:47:49 -0500

    Paquetes2.0

---
+++ JavaApplication25/src/zHotel/zHotel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zHotel;
+
+/**
+ *
+ * @author KEVIN SANCHEZ
+ */
+public class zHotel {
+    public static void main(String[] args) {
+        
+    }
+}
