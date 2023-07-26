56ff291cbea96d220e4feed0a5f03586364dca28
Jean15M <127127163+Jean15M@users.noreply.github.com>
2023-07-26 08:50:17 -0500

    ARREGLO ASIGNAR RESERVAS
    Se realizó los arreglos de asignar reservas.

---
+++ JavaApplication25/src/controlador/controladorPanelServicios.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import modelo.modeloServicio;
+import vista.PanelControlServ;
+
+/**
+ *
+ * @author Joseline
+ */
+public class controladorPanelServicios {
+    private modeloServicio modeloS;
+    private PanelControlServ vistaR;
+    private DefaultTableModel mTabla;
+
+    public controladorPanelServicios(modeloServicio modeloS, PanelControlServ vistaR) {
+        this.modeloS = modeloS;
+        this.vistaR = vistaR;
+        vistaR.setVisible(true);
+    }
+
+    
+    
+//    public void iniciarControlador(){
+//        vistaR.getBtnBuscarServi().addActionListener(l->cargarTabla());
+//    }
+//    
+//    public void cargarTabla(){
+//    if(vistaR.getCbServicios().getSelectedIndex()==0){
+//        JOptionPane.showMessageDialog(null, "SELECCIONE UNA OPCIÓN");
+//    }else{
+//        mTabla = (DefaultTableModel) vistaR.getTbServicios().getModel();
+//        mTabla.setNumRows(0);
+//        modeloS.listarServicio().stream().forEach(lista -> {
+//            String[] fila = {lista.getId_Servicio(), lista.getId_tipo_servicio(), lista.getId_Habitacion()};
+//            mTabla.addRow(fila);
+//        });
+//        vistaR.getTbServicios().setModel(mTabla);
+//     }
+//    }
+}
