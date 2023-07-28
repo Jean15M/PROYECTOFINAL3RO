1ee63c8d3e836f70984f8db73710433efbbeb2b5
Santana <carsantana54@gmail.com>
2023-07-28 12:23:58 -0500

    ...

--- JavaApplication25/src/controlador/controladorPanelconRecep.java
+++ JavaApplication25/src/controlador/controladorPanelconRecep.java
@@ -8,19 +8,7 @@ package controlador;
 import java.beans.PropertyVetoException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
-import modelo.modeloCliente;
-import modelo.modeloDetalle_fac;
-import modelo.modeloEncabez_fac;
-import modelo.modeloHabitaciones;
-import modelo.modeloParqueadero;
-import modelo.modeloServicio;
-import vista.PanelControlRecepcionista;
-import vista.PanelControlServ;
-import vista.v_recep_reserva;
-import vista.vistaPanelControl;
-import vista.vistaPanelControlPrincipal;
 import modelo.*;
 import vista.*;
 
@@ -37,19 +25,11 @@ public class controladorPanelconRecep {
     }
 
     public void iniciarControlador() {
-        //vistaRecepcionista.getBtnPerfil().setText(Controlador_Login.usuario);
-//        vistaRecepcionista.getBtnPerfil().addActionListener(l -> abrirDialogo());
         System.out.println("hola: " + Controlador_Login.usuario);
         vistaRecepcionista.getBtnInicioRe().addActionListener(l -> cerrar());
         vistapanel.getBtHabitaciones().addActionListener(l -> llamarPanelHp());
-
         vistapanel.getBtReservas().addActionListener(l -> llamarPanelReser());
         vistapanel.getBtServicios().addActionListener(l -> llamarPanelServi());
-
-        //vistapanel.getBtReservas().addActionListener(l -> llamarpa());
-        //vistaRecepcionista.getBtnModificar().addActionListener(l -> llamarPanleModificar());
-        //vistaRecepcionista.getBtnVerDatos().addActionListener(l -> verDatos());
-
     }
 
     private void cerrar() {
@@ -103,68 +83,4 @@ public class controladorPanelconRecep {
         iniciar.iniciarControlador();
         vistapanel.dispose();
     }
-    
-    public void abrirDialogo(){
-        //vistaRecepcionista.getjDialogDatos().setLocationRelativeTo(null);
-        //vistaRecepcionista.getjDialogDatos().setSize(384,212);
-        //vistaRecepcionista.getjDialogDatos().setVisible(true);
-    }
-    
-    public void llamarPanleModificar(){
-        modeloRecepcionista modelo = new modeloRecepcionista();
-        PanelModificarRecepcionista vista = new PanelModificarRecepcionista();
-        vistaRecepcionista.getjDesktopPane1().add(vista);
-        vista.setBorder(null);
-        BasicInternalFrameUI bui = (BasicInternalFrameUI) vista.getUI();
-        bui.setNorthPane(null);
-        vista.setSize(vistaRecepcionista.getjDesktopPane1().getWidth(), vistaRecepcionista.getjDesktopPane1().getHeight());
-        controlador_Mod_Recepcionista controlador = new controlador_Mod_Recepcionista(modelo, vista);
-        controlador.iniciarControl();
-    }
-    
-    public void verDatos(){
-        modeloRecepcionista modelo = new modeloRecepcionista();
-        PanelModificarRecepcionista vistaMod = new PanelModificarRecepcionista();
-        vistaMod.getTxtUsuario().setText(Controlador_Login.usuario);
-        vistaMod.getTxtUsuario().setEditable(false);
-        modelo.setUsuario_Recep(Controlador_Login.usuario);
-        vistaRecepcionista.getjDesktopPane1().add(vistaMod);
-        vistaMod.setBorder(null);
-        BasicInternalFrameUI bui = (BasicInternalFrameUI) vistaMod.getUI();
-        bui.setNorthPane(null);
-        vistaMod.setSize(vistaRecepcionista.getjDesktopPane1().getWidth(), vistaRecepcionista.getjDesktopPane1().getHeight());
-        controlador_Mod_Recepcionista controlador = new controlador_Mod_Recepcionista(modelo, vistaMod);
-        controlador.iniciarControl();
-        vistaMod.getBtnModificar().setVisible(false);
-        vistaMod.getBtnCancelar().setText("Regresar");
-        if(modelo.buscarRecepcionista().isEmpty()){
-          JOptionPane.showMessageDialog(null, "El cliente no se encuentra en la base de datos");
-        }else{
-           modelo.buscarRecepcionista().stream().forEach((p)->{
-           vistaMod.getTxtcedula().setText(p.getCedulaPersona());
-           vistaMod.getTxtcedula().setEditable(false);           
-           vistaMod.getTxtnom1().setText(p.getNombrePersona());
-           vistaMod.getTxtnom1().setEditable(false);
-           vistaMod.getTxtnom2().setText(p.getNombrePersona1());
-           vistaMod.getTxtnom2().setEditable(false);
-           vistaMod.getTxtape1().setText(p.getApellidoPersona());
-           vistaMod.getTxtape1().setEditable(false);
-           vistaMod.getTxtape2().setText(p.getApellidoPersona1());
-           vistaMod.getTxtape2().setEditable(false);
-           vistaMod.getTxttelefono().setText(p.getTelefonoPersona());
-           vistaMod.getTxttelefono().setEditable(false);
-           vistaMod.getTxtdireccion().setText(p.getDireccionPersona());
-           vistaMod.getTxtdireccion().setEditable(false);
-           vistaMod.getTxtcorreo().setText(p.getCorreoPersona());
-           vistaMod.getTxtcorreo().setEditable(false);
-           vistaMod.getTxtcontrasena().setText(p.getContra_Recep());
-           vistaMod.getTxtcontrasena().setEditable(false);
-           });
-        }
-    } 
 }
-
-    
-        
-
-
